bc87c341420786977d79dec2ea88267f6e834500
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-31 17:10:12 +0000

    Updating the MenuItemCollection Entity

--- src/Entities/MenuItemCollection.php
+++ src/Entities/MenuItemCollection.php
@@ -20,10 +20,36 @@ class MenuItemCollection extends Collection
      * @param  mixed  $key
      * @param  mixed  $default
      *
-     * @return \Arcanedev\Menus\Entities\MenuItem|null
+     * @return \Arcanedev\Menus\Entities\MenuItem|mixed
      */
     public function get($key, $default = null)
     {
         return parent::get($key, $default);
+    }
+
+    /**
+     * Get the first item from the collection.
+     *
+     * @param  callable|null  $callback
+     * @param  mixed          $default
+     *
+     * @return \Arcanedev\Menus\Entities\MenuItem|mixed
+     */
+    public function first(callable $callback = null, $default = null)
+    {
+        return parent::first($callback, $default);
+    }
+
+    /**
+     * Get the last item from the collection.
+     *
+     * @param  callable|null  $callback
+     * @param  mixed          $default
+     *
+     * @return \Arcanedev\Menus\Entities\MenuItem|mixed
+     */
+    public function last(callable $callback = null, $default = null)
+    {
+        return parent::last($callback, $default);
     }
 }
