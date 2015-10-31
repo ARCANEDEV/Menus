0978eecd7bd7fb295d1a6bf656b75d08a07e5b5d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-31 18:10:23 +0000

    Adding twitter bootstrap 3 mutli-level sub menus.

--- resources/views/bootstrap-3/dropdown-item.blade.php
+++ resources/views/bootstrap-3/dropdown-item.blade.php
@@ -3,7 +3,7 @@
         {{ $item->getContent() }} <span class="caret"></span>
     </a>
     @if ($item->hasChildren())
-        <ul class="{{ $item->isRoot() ? 'dropdown-menu' : 'dropdown-submenu' }}">
+        <ul class="{{ $item->isRoot() ? 'dropdown-menu' : 'dropdown-sub-menu' }}">
             @each('menus::bootstrap-3.menu-item', $item->children(), 'item')
         </ul>
     @endif
